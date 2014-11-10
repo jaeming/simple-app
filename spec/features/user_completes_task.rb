4b4b618512724609a771df818c2d8fa5f6982f7f
Benji dalton <daylightsavings@gmail.com>
2014-11-10 20:59:50 +1300

    updated features test for edit

--- spec/features/user_completes_task.rb
+++ spec/features/user_completes_task.rb
@@ -4,7 +4,7 @@ feature 'User creates task' do
   scenario 'Successfully' do
     task = create(:task)
     visit tasks_path
-    click_link("task-#{task.id}")
+    page.first(".tasks").click
     expect( page ).to have_content('Task complete!')
   end
 end
--- spec/features/user_edits_task.rb
+++ spec/features/user_edits_task.rb
@@ -4,7 +4,10 @@ feature "user edits task" do
   scenario "successfully" do
     task = create(:task)
     visit tasks_path
-
-
+    page.first(".task-icon-edit").click
+    fill_in 'Description', with: 'Cancel my perm.'
+    click_button 'Add'
+    visit tasks_path
+    expect( page ).to have_content('Cancel my perm.')
   end
 end
